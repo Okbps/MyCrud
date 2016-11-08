f09ef44091491da20655d0abfe48dc0fa15559e5
Yauhen <stan01@tut.by>
2016-11-08 22:25:32 +0300

    Update mysql_sripts.sql

--- mysql_sripts.sql
+++ mysql_sripts.sql
@@ -1,5 +1,5 @@
 USE test;
-CREATE TABLE user ( id int(8) NOT NULL AUTO_INCREMENT, name varchar(25), age int, isAdmin bit, createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE user ( id int(8) NOT NULL AUTO_INCREMENT, name varchar(25), age int, isAdmin bit, createDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 insert into user (name, age, isAdmin) values ('Barak.O', 44, 0);
 insert into user (name, age, isAdmin) values ('Keane', 30, 0);
 insert into user (name, age, isAdmin) values ('Tom', 45, 0);
